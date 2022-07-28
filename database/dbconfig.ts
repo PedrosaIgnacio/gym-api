9a8828d3768d9545ef60487d9c038ffa10d86a74
JGUAL <joaquingual1@gmail.com>
2022-07-28 02:24:40 -0300

    User Controller added
    Chage db config to Joa's Server

--- database/dbconfig.ts
+++ database/dbconfig.ts
@@ -1,8 +1,8 @@
 export const config = {
-  user: "nacho",
-  password: "12345",
+  user: "sa",
+  password: "123456",
   //   server: "DESKTOP-AJIOCC9\\SQLEXPRESS",
-  server: "DESKTOP-AJIOCC9\\SQLEXPRESS",
+  server: "JGUAL\\JOAQUIN",
   database: "Gimnasio",
   options: {
     trustServerCertificate: false, // change to true for local dev / self-signed certs
